349e0a6ed486e50bd572aea60027521365d42dd8
Jan Galinski <jan.galinski@holisticon.de>
2018-10-31 12:35:11 +0100

    eval logic works

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    application
+    kotlin("jvm") version "1.3.0"
+}
+
+application {
+  mainClassName = "com.github.jangalinski.tidesoftime.ApplicationKt"
+}
+
+dependencies {
+  compile(kotlin("stdlib-jdk8"))
+  compile(kotlin("reflect"))
+
+
+  testCompile("org.junit.jupiter:junit-jupiter-api:5.3.1")
+  testCompile("org.junit.jupiter:junit-jupiter-params:5.3.1")
+  testRuntime("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+  testCompile("org.assertj:assertj-core:3.11.1")
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
+  testLogging {
+    events("passed", "skipped", "failed")
+  }
+}
+
+repositories {
+    jcenter()
+}
